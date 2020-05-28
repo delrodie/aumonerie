420229669de79609c0ed4b90f9f6ec59e9ace9b1
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-05-28 09:51:01 +0000

    Backoffice gestion des types

---
+++ src/Controller/TypeController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Type;
+use App\Form\TypeType;
+use App\Repository\TypeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/backend/type")
+ */
+class TypeController extends AbstractController
+{
+    /**
+     * @Route("/", name="type_index", methods={"GET","POST"})
+     */
+    public function index(Request $request, TypeRepository $typeRepository): Response
+    {
+        $type = new Type();
+        $form = $this->createForm(TypeType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Verificatio de l'existence
+            $verif = $typeRepository->findOneBy(['libelle'=>$type->getLibelle()]);
+            if ($verif){
+                $this->addFlash('danger', "Ce type existe déjà");
+                return $this->redirectToRoute('type_index');
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($type);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('type_index');
+        }
+
+        return $this->render('type/index.html.twig', [
+            'types' => $typeRepository->findBy([],['libelle'=>'ASC']),
+            'type' => $type,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="type_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $type = new Type();
+        $form = $this->createForm(TypeType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($type);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('type_index');
+        }
+
+        return $this->render('type/new.html.twig', [
+            'type' => $type,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="type_show", methods={"GET"})
+     */
+    public function show(Type $type): Response
+    {
+        return $this->render('type/show.html.twig', [
+            'type' => $type,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="type_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Type $type, TypeRepository $typeRepository): Response
+    {
+        $form = $this->createForm(TypeType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', "Le type a bien été modifié");
+
+            return $this->redirectToRoute('type_index');
+        }
+
+        return $this->render('type/edit.html.twig', [
+            'types' => $typeRepository->findBy([],['libelle'=>'ASC']),
+            'type' => $type,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="type_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Type $type): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$type->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($type);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('type_index');
+    }
+}
