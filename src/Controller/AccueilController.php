148c5a2eca5afbf2eb092cea7cdf27cbaca56bd5
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-05-29 22:09:53 +0000

    Frontend Accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualiteRepository;
+use App\Repository\CardinalRepository;
+use App\Repository\PresentationRepository;
+use App\Repository\SliderRepository;
 use App\Utilities\GestionLog;
 use App\Utilities\GestionMail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,20 +26,35 @@ class AccueilController extends AbstractController
     /**
      * @Route("/", name="app_accueil")
      */
-    public function index()
+    public function index(PresentationRepository $presentationRepository, CardinalRepository $cardinalRepository, SliderRepository $sliderRepository, ActualiteRepository $actualiteRepository)
     {
+
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'cardinal' => $cardinalRepository->findOneBy([],['id'=>"DESC"]),
+            'vision' => $presentationRepository->findOneBy(['rubrique'=>"vision"]),
+            'actualites' => $actualiteRepository->findBy([],['id'=>'DESC']),
+            'sliders' => $sliderRepository->findBy([],['id'=>'DESC'])
         ]);
     }
 
     /**
-     * @Route("/dashboard", name="app_dashboard")
+     * @Route("/menu", name="app_menu")
+     */
+    public function menu(PresentationRepository $presentationRepository)
+    {
+        return $this->render('accueil/menu.html.twig',[
+            'presentations'=> $presentationRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/backend/dashboard", name="app_dashboard")
      */
     public function dashboard(Request $request)
     {
-        $user = $this->getUser();
-        $this->log->addLog($user, 'dashboard', $request->getClientIp());
-        return $this->render("accueil/index.html.twig");
+        // Creation du log
+        $action = "Anonyme a tenté de se connecter au backoffice";
+        $this->log->addLog($action);
+        return $this->render("accueil/dashbord.html.twig");
     }
 }
