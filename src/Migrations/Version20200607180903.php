a1796ee38c08a69fe5ad9cf7d59c8b50eefacab9
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-06-08 12:03:03 +0000

    MAJ Version

--- src/Migrations/Version20200607180903.php
+++ src/Migrations/Version20200607180903.php
@@ -22,7 +22,7 @@ final class Version20200607180903 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE departement ADD slug VARCHAR(255) DEFAULT NULL, CHANGE description contenu LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE departement ADD slug VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200607180903 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE departement DROP slug, CHANGE contenu description LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE departement DROP slug');
     }
 }
