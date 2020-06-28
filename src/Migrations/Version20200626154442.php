58397a1a0415f166e32cc5c7e65d44e85dce731e
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-06-28 14:29:15 +0000

    MAJ migrations

--- src/Migrations/Version20200626154442.php
+++ src/Migrations/Version20200626154442.php
@@ -22,7 +22,6 @@ final class Version20200626154442 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE presentation (id INT AUTO_INCREMENT NOT NULL, rubrique VARCHAR(255) NOT NULL, titre VARCHAR(255) NOT NULL, contenu LONGTEXT NOT NULL, photo VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT NULL, created_by VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE programme_activite (id INT AUTO_INCREMENT NOT NULL, programme_id INT DEFAULT NULL, date VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, lieu VARCHAR(255) NOT NULL, reportage VARCHAR(255) DEFAULT NULL, INDEX IDX_C2C4ACF062BB7AEE (programme_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE programme_activite ADD CONSTRAINT FK_C2C4ACF062BB7AEE FOREIGN KEY (programme_id) REFERENCES programme (id)');
         $this->addSql('ALTER TABLE departement CHANGE contenu contenu LONGTEXT DEFAULT NULL');
@@ -34,7 +33,6 @@ final class Version20200626154442 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE presentation');
         $this->addSql('DROP TABLE programme_activite');
         $this->addSql('ALTER TABLE departement CHANGE contenu contenu LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
