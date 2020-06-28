31e18c996db761a7214b9160e6cc2ec8f88e87f9
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-06-28 14:31:09 +0000

    MAJ migrations

--- src/Migrations/Version20200626154442.php
+++ src/Migrations/Version20200626154442.php
@@ -25,7 +25,6 @@ final class Version20200626154442 extends AbstractMigration
         $this->addSql('CREATE TABLE programme_activite (id INT AUTO_INCREMENT NOT NULL, programme_id INT DEFAULT NULL, date VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, lieu VARCHAR(255) NOT NULL, reportage VARCHAR(255) DEFAULT NULL, INDEX IDX_C2C4ACF062BB7AEE (programme_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE programme_activite ADD CONSTRAINT FK_C2C4ACF062BB7AEE FOREIGN KEY (programme_id) REFERENCES programme (id)');
         $this->addSql('ALTER TABLE departement CHANGE contenu contenu LONGTEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -35,6 +34,5 @@ final class Version20200626154442 extends AbstractMigration
 
         $this->addSql('DROP TABLE programme_activite');
         $this->addSql('ALTER TABLE departement CHANGE contenu contenu LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
